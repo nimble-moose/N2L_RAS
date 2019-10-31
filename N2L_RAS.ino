7a9fb1b6480a1aed52b174bfa8da83caca07940c
Ben Wilhelm <benjamin.m.wilhelm@gmail.com>
2019-10-31 15:13:12 -0500

    Rewrite to actually be receiver
    accidentally saved transmitter in initial commit

--- N2L_RAS.ino
+++ N2L_RAS.ino
@@ -1,42 +1,47 @@
-// ask_transmitter.pde
+// ask_receiver.pde
 // -*- mode: C++ -*-
-// Simple example of how to use RadioHead to transmit messages
+// Simple example of how to use RadioHead to receive messages
 // with a simple ASK transmitter in a very simple way.
-// Implements a simplex (one-way) transmitter with an TX-C1 module
+// Implements a simplex (one-way) receiver with an Rx-B1 module
 // Tested on Arduino Mega, Duemilanova, Uno, Due, Teensy, ESP-12
 
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 
-RH_ASK driver;
-// RH_ASK driver(2000, 4, 5, 0); // ESP8266 or ESP32: do not use pin 11 or 2
+RH_ASK driver(2000, 8); // ESP8266 or ESP32: do not use pin 11 or 2
 
-int lastState = 0;
-const int SWITCH_PIN = 8;
+int ledState = 0;
+const int LED_PIN = 9;
 
 void setup()
 {
-    Serial.begin(9600);	  // Debugging only
-    if (!driver.init()) {
+    Serial.begin(9600);	// Debugging only
+    if (!driver.init())
          Serial.println("init failed");
-    } else {
-         Serial.println("transmitter initialized");
-    }
 
-    pinMode(SWITCH_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+
+    digitalWrite(LED_PIN, 1);
+    delay(1000);
+    digitalWrite(LED_PIN, ledState);
 }
 
 void loop()
 {
-    
-    int switchState = !digitalRead(SWITCH_PIN);
-//    if (switchState != lastState) {
-      const char *msg = switchState ? "ras:1;" : "ras:0;";
-      Serial.print("sending message: ");
-      Serial.println(msg);
-      driver.send((uint8_t *)msg, strlen(msg));
-      driver.waitPacketSent();
-      lastState = switchState;
-//    }
-    delay(200);
+    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    uint8_t buflen = sizeof(buf);
+
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+    	char* command = strtok((char*)buf, ":");
+      char* argument = strtok(NULL, ";");
+
+      if (strcmp(command, "ras") == 0) {
+        int enabled = strcmp(argument, "1") == 0;
+        if (enabled != ledState) {
+          digitalWrite(LED_PIN, enabled);
+          ledState = enabled;
+        }
+      }
+    }
 }
